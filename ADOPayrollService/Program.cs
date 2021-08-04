2832cd3cac1bf0a325ddedc1a17583e8fdb6f395
Vedha <vedhavel.1234@gmail.com>
2021-08-04 20:28:32 +0530

    [Vedhashni] Updated the file with payroll service

---
+++ ADOPayrollService/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOPayrollService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to payroll service");
+        }
+    }
+}
